c8815dae8a4768640e3a3e7721313f862ebfcd32
Andrew <andrewnijmeh1@gmail.com>
2022-05-29 16:12:38 -0400

    feat: (base boilerplate)

---
+++ beam-mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  beam-mobile
+//
+//  Created by Andrew Nijmeh on 2022-05-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Beam")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ beam-mobile/beam_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  beam_mobileApp.swift
+//  beam-mobile
+//
+//  Created by Andrew Nijmeh on 2022-05-29.
+//
+
+import SwiftUI
+
+@main
+struct beam_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
